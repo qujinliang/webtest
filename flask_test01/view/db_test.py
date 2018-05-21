ae3acf238c60c5a949e7384dff7c315c8b07a97e
qujinliang <qujin_liang@163.com>
2018-05-21 18:57:00 +0800

    ""

--- flask_test01/view/db_test.py
+++ flask_test01/view/db_test.py
@@ -3,13 +3,14 @@ import pymysql
 
 class DbTest:
     def __init__(self):
-        self.db = pymysql.connect("localhost","root","root","test01")
+        self.db = pymysql.connect("localhost","root","","test01")
         self.cursor = self.db.cursor()
 
-
-    def db_data(self,name):
-        sql = "SELECT * FROM EMPLOYEE WHERE INCOME > '%s' " %(name)
-
+    def db_data(self,name,age):
+        if name:
+            sql = "SELECT * FROM user WHERE name = '%s' " %(name)
+        if age:
+            sql = "SELECT * FROM user WHERE name = '%d' " % (age)
         try:
             self.cursor.execute(sql)
             result = self.cursor.fetchall()
