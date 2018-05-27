d4c979924f1d432f4775c56fee72eeadfc8cf836
qujinliang <qujin_liang@163.com>
2018-05-27 23:06:45 +0800

    'blog'

--- blog/__init__.py
+++ blog/__init__.py
@@ -8,9 +8,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 # 创建项目对象
 app = Flask(__name__)
+
 # 加载配置文件内容
 app.config.from_object('blog.setting')      # 模块下的setting文件名，不用加py后缀
 app.config.from_envvar('FLASK_SETTINGS')    # 环境变量，只想配置文件setting的路径
 
 # 创建数据库对象
 db = SQLAlchemy(app)
+from blog.model import User,Category
--- blog/setting.py
+++ blog/setting.py
@@ -5,9 +5,9 @@ __author__ = 'qujl'
 # 调试模式是否开启
 DEBUG = True
 
-SQLALCHEMY_TRACK_MODIFCATIONS = False
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 # session必须要设置key
 SECRET_KEY = 'zx1efSNdO0yV9LGp'
 
 # mysql数据库连接信息，这里改成自己的账号
-SQLALCHEMY_DATABASE_URL = 'mysql://root:root@localhost/3306/blog'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:root@127.0.0.1:3306/blog'
